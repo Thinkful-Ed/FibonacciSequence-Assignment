cd12a4ead02ab760f4a3c0665577b8b6dd1a9c74
Brian Schachter <brian@twosixtytwo.com>
2014-09-16 02:12:45 -0500

    Mofifying README.md

---
+++ FibonacciSequence-Assignment.playground/section-1.swift
@@ -0,0 +1,28 @@
+// Thinkful Playground
+// Thinkful.com
+
+// Fibonacci Sequence
+
+// By definition, the first two numbers in the Fibonacci sequence are 1 and 1, or 0 and 1, depending on the chosen starting point of the sequence, and each subsequent number is the sum of the previous two.
+
+import UIKit
+
+class FibonacciSequence {
+    
+    let includesZero: Bool
+    let values: [Int]
+    
+    init(maxNumber: Int, includesZero: Bool) {
+        self.includesZero = includesZero
+        //TODO: Create an array which contains the numbers in the Fibonacci sequence, but don't add any numbers to the array which exceed the maxNumber. For example, if the maxNumber is 10 then the array should contain [0,1,1,2,3,5,8] because the next number is 13 which is higher than the maxNumber. If includesZero is false then you should not include the number 0 in the sequence.
+    }
+    
+    init(numberOfItemsInSequence: Int, includesZero: Bool) {
+        self.includesZero = includesZero
+        //TODO: Create an array which contains the numbers in the Fibonacci sequence, and the array should contain this many items: numberOfItemsInSequence. For example, if numberOfItemsInSequence is 10 then the array should contain [0,1,1,2,3,5,8,13,21,34] if inlcudesZero is true, or [1,1,2,3,5,8,13,21,34,55] if includesZero is false.
+    }
+}
+
+let fibanocciSequence = FibonacciSequence(maxNumber:12345, includesZero: true)
+
+let anotherSequence = FibonacciSequence(numberOfItemsInSequence: 13, includesZero: true)
